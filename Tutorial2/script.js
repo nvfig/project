ea9af972500d0d63c0228b60e95bb172e5c11cf1
Ivan <example@gmail.com>
2021-07-13 13:56:21 +0500

    first commit

---
+++ Tutorial2/script.js
@@ -0,0 +1,21 @@
+const bool = false;
+
+console.log(something);
+
+let und;
+
+console.log(und);
+
+const obj ={
+    name: "John", 
+    age: 25,
+    isMarried: false
+};
+
+
+console.log(obj['name']);
+
+let arr =['plum.png', 'orange.jpg', 'apple.bmp'];
+
+console.log(arr[1]);
+
